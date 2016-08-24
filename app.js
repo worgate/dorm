4cfb3f74b139c123e3d59c056404cfa886fbb966
Chinwat k <appleworgate@outlook.com>
2016-08-24 14:50:13 +0000

    add home

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var  express    = require("express"),
      app        = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
 
 app.get("/", function(req, res){
-    res.send("EDIT CAMPGROUNds") 
+    res.render("welcome");
 });
+
+
+app.get("/home", function(req, res){
+    res.send("home");
+});
+
 
 
 
